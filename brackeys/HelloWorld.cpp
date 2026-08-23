bd153783ca8eb2b05f98069f7f3b2b976387ee6f
D2Lx0wse <92229936+D2Lx0wse@users.noreply.github.com>
2026-08-23 17:42:44 +0200

    make basic window

--- brackeys/HelloWorld.cpp
+++ brackeys/HelloWorld.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-
+#include "raylib.h"
 
 int main() {
-	std::cout << "Hello, world!";
+	constexpr int screenWidth{800};
+	constexpr int screenHeigth{600};
+
+	InitWindow(screenWidth, screenHeigth, "brackeys 2026 2");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+
+
+
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		EndDrawing();
+	}
+
+
 	return 0;
 }
