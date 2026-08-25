61a0d0a9a812bc00241610b21092e6405b02c732
Anton <dbxenoverse06@gmail.com>
2026-08-25 18:06:42 +0200

    Added operator+ and operator+= overloads between two Vector2

---
+++ brackeys/Vector2Overloads.cpp
@@ -0,0 +1,12 @@
+#include "Vector2Overloads.hpp"
+
+Vector2 operator+(const Vector2& a, const Vector2& b) {
+	return Vector2{ a.x + b.x, a.y + b.y };
+}
+
+Vector2& operator+=(Vector2& a, const Vector2& b) {
+	a.x += b.x;
+	a.y += b.y;
+
+	return a;
+}
---
+++ brackeys/Vector2Overloads.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <raylib.h>
+
+Vector2 operator+(const Vector2& a, const Vector2& b);
+
+Vector2& operator+=(Vector2& a, const Vector2& b);
